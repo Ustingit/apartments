2c7788dedb1c9e21789d439f5e73b9ce2c19e882
Ustin <ustinx@mail.ru>
2018-05-13 22:20:00 +0300

    two views using render

--- flats/views.py
+++ flats/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from .models import Flat
 
 
 def index(request):
     latest_flats_list = Flat.objects.order_by('-pub_date')[:6]
-    output = ', '.join([flat.about for flat in latest_flats_list])
-    return HttpResponse(output)
+    context = {'latest_flats_list': latest_flats_list}
+    return render(request, 'flats/index.html', context)
 
 
 def detail(request, flat_id):
-    return HttpResponse("You're looking at flat %s." % flat_id)
+    flat = get_object_or_404(Flat, pk=flat_id)
+    return render(request, 'flats/detail.html', {'flat': flat})
