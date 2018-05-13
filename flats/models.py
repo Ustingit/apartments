c08d10113429239f6de3156277ce11ac33d9ecfb
Ustin <ustinx@mail.ru>
2018-05-13 19:19:29 +0300

    first migrate and model

--- flats/models.py
+++ flats/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Flat(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    flat_url = models.CharField(max_length=200)
+    flat_id = models.IntegerField()
+    price = models.CharField(max_length=50)
+    phone_text = models.CharField(max_length=25)
+    phone_img = models.ImageField()
+    flat_name = models.CharField(max_length=200)
+    owner = models.CharField(max_length=100)
+    about = models.CharField(max_length=1000)
